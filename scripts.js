6392c08e3cc1a72567837efcabddf5003fb2fb97
gabrielfari4 <gabrielfaria88@gmail.com>
2024-12-18 16:55:59 -0300

    :sparkles: feat: lógica leitura de arquivo

--- scripts.js
+++ scripts.js
@@ -5,7 +5,10 @@ uploadBtn.addEventListener('click', () => {
     inputUpload.click()
 })
 
-inputUpload.addEventListener('change', function(event) {
+const mainImage = document.querySelector('.main-imagem');
+const imageName = document.querySelector('.container-imagem-nome p');
+
+inputUpload.addEventListener('change', async (event) => {
     let file = event.target.files[0];
     if (!file.type.match('image/png') && !file.type.match('image/jpeg')) {
         alert('Por favor, selecione uma imagem PNG ou JPEG.');
@@ -18,6 +21,31 @@ inputUpload.addEventListener('change', function(event) {
         inputUpload.value = ''; // Reseta o valor do input
         return;
     }
+
+    if (file) {
+        try {
+            const fileContent = await readFileContent(file);
+            mainImage.src = fileContent.url;
+            imageName.textContent = fileContent.nome;
+        } catch (e) {
+            console.error(e)
+        }
+    }
     
     console.log(file);
 });
+
+const readFileContent = (file) => {
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.onload = () => {
+            resolve({ url: leitor.result, nome: file.name})
+        }
+
+        leitor.onerror = () => {
+            reject(`Erro na leitura do arquivo ${file.name}`)
+        } 
+
+        leitor.readAsDataURL(file)
+    });
+}
